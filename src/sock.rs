9b74453d1f86cb06f413ed178a4871001ad5ec39
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-11-17 17:05:15 -0600

    Add SocketWriter
    This doesn't work very well yet but it's coming along.
    Also fixed a few bugs.

--- src/sock.rs
+++ src/sock.rs
@@ -1,11 +1,31 @@
 use super::thread::ThreadPool;
 use std::{
     collections::VecDeque,
-    io::Write,
+    io::{self, Write},
     marker::PhantomData,
     net::{TcpStream, ToSocketAddrs},
     sync::{Arc, Mutex},
 };
+
+pub struct SocketWriter<S, F> {
+    pool: SocketPool<S, F>,
+}
+
+impl<S, F> Write for SocketWriter<S, F>
+where
+    S: ToSocketAddrs + Send + Clone + 'static,
+    F: FnOnce() + 'static,
+{
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        for i in 0..buf.len() {
+            // Put all the bytes in `buf` into the queue
+            self.pool.enqueue(Box::new([buf[i]])); // This needs to be optimized, but it'll work
+        }
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> { Ok(()) }
+}
 
 pub struct SocketPool<S, F> {
     connections: Vec<Connection<S, F>>,
@@ -19,13 +39,18 @@ where
 {
     pub fn new(size: usize, ip: S) -> SocketPool<S, F> {
         debug_assert!(size > 0);
+        // Initialize the queue
         let queue = Arc::new(Mutex::new(Queue::<Message<Box<[u8]>>>::new()));
 
+        // Initialize the thread pool
         let pool = Arc::new(Mutex::new(ThreadPool::new(size)));
 
-        let mut connections = Vec::new();
+        // Push out some Connections, with Arc clones of the queue and thread
+        // pool
+        let mut connections = Vec::with_capacity(size);
         for _ in 0..size {
             connections.push(Connection::new(
+                // TODO: connect to multiple socket (ie all of them?)
                 ip.clone(),
                 Arc::clone(&queue),
                 Arc::clone(&pool),
@@ -33,6 +58,22 @@ where
         }
 
         SocketPool { connections, queue }
+    }
+
+    pub fn size(&self) -> usize { self.connections.len() }
+
+    pub fn enqueue(&mut self, buf: Box<[u8]>) {
+        // Lock onto the queue before pushing to it
+        (*self.queue.lock().expect("Could not get a lock on queue"))
+            .push(Message::Data(buf))
+    }
+}
+
+impl<S, F> Drop for SocketPool<S, F> {
+    fn drop(&mut self) {
+        for _ in 0..self.connections.len() {
+            (*self.queue.lock().unwrap()).push(Message::Terminate);
+        }
     }
 }
 
@@ -48,19 +89,20 @@ where
 {
     pub fn new(
         ip: S,
-        queue: Arc<Mutex<Queue<Message<Box<[u8]>>>>>, /* BUG: this will not
-                                                       * have a static
-                                                       * lifetime */
+        queue: Arc<Mutex<Queue<Message<Box<[u8]>>>>>,
         pool: Arc<Mutex<ThreadPool>>,
     ) -> Connection<S, F> {
-        // (*pool.lock().expect("Lock failed"));
         match pool.lock() {
             Ok(i) => {
+                // Ask the thread pool to execute this
                 i.execute(move || {
+                    // Try to establish a TCP connection
                     let mut stream = {
                         let mut loop_count = 0;
                         loop {
+                            // Five tries
                             if loop_count > 5 {
+                                // Give up
                                 println!("Could not connect. Giving up...");
                                 return;
                             } else {
@@ -77,8 +119,6 @@ where
                             }
                         }
                     };
-                    // TcpStream::connect(ip)
-                    // .expect("Failed to initialize connection");
                     loop {
                         // If there's a message in the queue, depending on the
                         // type...
@@ -91,7 +131,7 @@ where
                                         .expect("Could not write to stream");
                                 },
                                 // or terminate this connection.
-                                Message::Terminate => break,
+                                Message::Terminate => break, // Should be `return;`?
                             }
                         }
                     }
@@ -127,9 +167,9 @@ impl<T> Queue<T> {
 
     pub fn is_empty(&self) -> bool { self.inner.is_empty() }
 
-    pub fn pop(&mut self) -> Option<T> { self.inner.pop_back() }
+    pub fn pop(&mut self) -> Option<T> { self.inner.pop_front() }
 
-    pub fn push(&mut self, item: T) { self.inner.push_front(item) }
+    pub fn push(&mut self, item: T) { self.inner.push_back(item) }
 }
 
 enum Message<T> {
